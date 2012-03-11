e2a62a664a7417ec76f928ef1e781f845db444b6
Thomas Chou <thethomaschou@gmail.com>
2012-03-10 17:13:01 -0800

    first commit

---
+++ app/controllers/lessons_controller.rb
@@ -0,0 +1,7 @@
+class LessonsController < ApplicationController
+
+  def index
+    render 'index'
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fsd::Application.config.secret_token = '899669cb86a016727c35267e080347cd7f335f63e2e52eae54cd4fd6667a668628353e33e83c1ca479aba04d0c2294163e50282d7db56101b53f5107eb942e7a'

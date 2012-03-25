1650598194521eb3e4983ee1af7f3156a0c8ca5e
Marvin T <nivram@berkeley.edu>
2012-03-25 16:59:48 -0700

    uses mock instead

--- spec/controllers/lessons_controller_spec.rb
+++ spec/controllers/lessons_controller_spec.rb
@@ -63,13 +63,13 @@ describe LessonsController do
 
   describe 'add video to a lesson' do
     before :each do
-      @Lesson1 = new Lesson
-      @Lesson2 = new Lesson
-      @testUrl = "http://www.youtube.com/watch?v=dQw4w9WgXcQ&ob=av2n"
+      @Lesson1 = mock("Lesson", :id => 1)
+      @testUrl = "test URL"
     end
 
     it 'should add videos with valid urls' do
       Video.stub(:isValidUrl?).and_return(true)
+      Lesson.stub(:find).with(@Lesson1.id).and_return(@Lesson1)
       post :addVideo, {:lesson_id => @Lesson1.id, :video_url => testUrl}
       @Lesson1.components.empty?.should be(false)
       #should replace with better test once we know how the controller method works
@@ -77,14 +77,16 @@ describe LessonsController do
 
     it 'should not add videos with invalid urls' do
       Video.stub(:isValidUrl?).and_return(false)
-      @Lesson2.addVideo(@testUrl)
+      Lesson.stub(:find).with(@Lesson1.id).and_return(@Lesson1)
+      post :addVideo, {:lesson_id => @Lesson1.id, :video_url => testUrl}
       @Lesson2.components.empty?.should be(true)
     end
 
-    it 'should show the Edit Lesson view' do
+    after :each do
       response.should render_template('edit_lesson')
     end
   end
   
 end
 
+  

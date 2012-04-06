03828a5fa01f6e3002f867f133cbeaf6ca70e5b3
Lingfeng <hlf0319@berkeley.edu>
2012-04-05 21:01:13 -0700

    change update

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -15,21 +15,41 @@ class LessonsController < ApplicationController
   end
 
   def create
-    @lesson = Lesson.create!(params[:lesson])
-    flash[:notice] = "#{@lesson.title} was successfully created."
-    redirect_to lessons_path
+    @lesson = Lesson.new(params[:lesson])
+    if request.post?
+      if @lesson.save 
+        flash[:notice] = "#{@lesson.title} was successfully created."
+        redirect_to lessons_path
+      else
+        flash[:notice] = "An error has happened."
+        redirect_to new_lesson_path
+      end  
+    end  
   end
 
   def edit
     @lesson = Lesson.find params[:id]
   end
-
+ 
   def update
     @lesson = Lesson.find params[:id]
-    @lesson.update_attributes!(params[:lesson])
-    flash[:notice] = "#{@lesson.title} was successfully updated."
-    redirect_to lesson_path(@lesson)
+    if request.post?
+      if Lesson.update(params[:id], params[:lesson])
+        flash[:notice] = "#{@lesson.title} was successfully updated."
+        redirect_to lesson_path(@lesson)
+      else
+        flash[:notice] = "An error has happened."
+        redirect_to edit_lesson_path(@lesson)
+      end
+    end
   end
+
+  # def update
+    # @lesson = Lesson.find params[:id]
+    # @lesson.update_attributes!(params[:lesson])
+    # flash[:notice] = "#{@lesson.title} was successfully updated."
+    # redirect_to lesson_path(@lesson)
+  # end
 
   def destroy
     @lesson = Lesson.find(params[:id])

d50fb5d703ffd80365c80a061ca910b53c293f6b
Lingfeng <hlf0319@berkeley.edu>
2012-04-08 19:26:49 -0700

    lesson cucumber still not work

--- features/step_definitions/lesson_steps.rb
+++ features/step_definitions/lesson_steps.rb
@@ -4,9 +4,26 @@ Given /^I enter the url for "([^"]*)"$/ do |url|
   pending # express the regexp above with the code you wish you had
 
 end
+# 
+# Given /^I am logged in$/ do
+  # Given %{I am on the login page}
+   # fills_in("Email", :with => "marvin")
+   # fills_in("Passwd", :with => "marvin2012")
+   # clicks_button("signIn")
+# end
 
+Given /^I try to login$/ do 
+  begin
+    fill_in("Email", :with => "marvin")
+    fill_in("Passwd", :with => "marvin2012")
+    click_link("signIn")
+  rescue
+  end
+end
 
-# Add a declarative step here for populating the DB with movies.
+Given /^I am logged in$/ do
+  controller.session[:admin_user] = true
+end
 
 Given /the following lessons exist/ do |lessons_table|
   lessons_table.hashes.each do |lesson|
@@ -14,7 +31,6 @@ Given /the following lessons exist/ do |lessons_table|
     # you should arrange to add that movie to the database here.
     Lesson.create!(lesson)
   end
-
 end
 
 Then /I should see all of the lessons/ do

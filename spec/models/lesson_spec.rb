de171b9ba656c6e14b7c9691e8faf2a01cb00322
Thomas Chou <thethomaschou@gmail.com>
2012-04-08 21:55:19 -0700

    added lesson model spec

--- spec/models/lesson_spec.rb
+++ spec/models/lesson_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
 describe Lesson do
-
+ describe 'check title' do
+   it 'should return title of the movie' do
+     fake_lesson = mock('Lesson1', :id => '1')
+     Lesson.stub(:find).with("1").
+       and_return(fake_lesson)
+     fake_lesson.should_receive(:title).
+       and_return('Lesson1')
+     Lesson.title("1")
+   end
+ end
 end

90ff92e7885df9f0922a785933baeaffcb594d65
Kenny Shiu <k.y.shiu@berkeley.edu>
2012-04-08 17:17:01 -0700

    fixed video spec

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -4,12 +4,12 @@ describe Video do
 
   describe 'Checking for valid videos' do
     it 'should return true if the url is valid' do
-      testUrl = '<iframe width="420" height="315" src="http://www.youtube.com/embed/dQw4w9WgXcQ" frameborder="0" allowfullscreen></iframe>'
+      testUrl = "http://youtu.bedQw4w9WgXcQ"
       Video.isValidUrl?(testUrl).should be(true)
     end
 
     it 'should reject invalid urls' do
-      badUrl = '<iframe width="420" height="315" src="http://www.blutube.com/embed/dQw4w9WgXcQ" frameborder="0" allowfullscreen></iframe>'
+      badUrl = "http://www.youtube.com/embed/dQw4w9WgXcQ"
       Video.isValidUrl?(badUrl).should be(false)
     end
   end
@@ -17,7 +17,7 @@ describe Video do
   describe 'Extracting embedable url' do
     it 'should extract the main part of the url from the extra formatting' do
       embed_url = 'http://www.youtube.com/embed/dQw4w9WgXcQ'
-      full_url = '<iframe width="420" height="315" src="http://www.youtube.com/embed/dQw4w9WgXcQ" frameborder="0" allowfullscreen></iframe>'
+      full_url = "http://youtu.be/dQw4w9WgXcQ"
       Video.embedableUrl(full_url).should == embed_url
     end
   end

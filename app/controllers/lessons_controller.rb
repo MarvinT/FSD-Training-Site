eb439d4314bd8a122c8963e03ae0ddbad3fb8b3c
Lingfeng <hlf0319@berkeley.edu>
2012-04-13 14:28:00 -0700

    comment rspec finish

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -99,4 +99,18 @@ class LessonsController < ApplicationController
     redirect_to lessons_path
   end
 
+  def create_comment
+    
+  end
+  def edit_comment
+    
+  end
+  
+  def update_comment
+    
+  end
+  
+  def destroy_comment
+    
+  end
 end

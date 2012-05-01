40c48a7234d37b46dc277503cfba1963e72c0cd9
Thomas Chou <thethomaschou@gmail.com>
2012-05-01 01:26:52 -0700

    reorder lessons

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -109,9 +109,6 @@ class LessonsController < ApplicationController
 
   def reorder
     @user = session[:admin_user]
-    banners = ["banner1.jpg", "banner2.jpg", "banner3.jpg", "banner4.jpg", "banner5.jpg", "banner6.jpg", "banner7.jpg", "banner8.jpg"]
-    random = rand(8)
-    @banner = banners[random]
     @lessons = Lesson.order(:position)
   end
 
